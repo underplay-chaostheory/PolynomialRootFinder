4fd0e5ad4d998832f701b79822f5aafaeb00b23b
DECARRE Damien <96182768+underplay-chaostheory@users.noreply.github.com>
2025-07-25 20:59:49 +0200

    Now used w/ v2 of test files

--- test_ud_dist.c
+++ test_ud_dist.c
@@ -36,9 +36,9 @@ int all_found(polynomial* p, polynomial* dp, complexe** roots, int nb_root_found
                 k++;
             } while (k < ITERATION_LOOP && distance(z0, prev) > tol);
             
-            if(nb_root_found < nb_root && distance(z0, prev) <= tol){
+            if(nb_root_found < nb_root && distance(z0, prev) > tol){
                 completed++;
-                nb_root_found = add_to_set(roots, z0, nb_root_found, EPS);
+                nb_root_found = add_to_set(roots, z0, nb_root_found, 2*EPS);
             } else {
                 replace_complexe(aux[aux_size], z0);
                 aux_size ++;
@@ -68,7 +68,7 @@ int root_finder(char* TEST_FILE, FILE* OUTPUT_FILE, int ITERATION_LOOP, double E
     test* t = get_test_file(TEST_FILE);
 
     for(int test = 0; test < t->nb_test; test ++){
-        polynomial* p = get_polynomial(t);
+        polynomial* p = get_polynomial_v2(t);
 
         if(p->degree < 3){
             printf("Erreur dans get_test : %d\n", p->degree);
@@ -108,7 +108,7 @@ int root_finder(char* TEST_FILE, FILE* OUTPUT_FILE, int ITERATION_LOOP, double E
             if(found == 0){
                 break;
             }
-            polynomial* root_found_prod = develop(&roots[nb_root_found], found);
+            polynomial* root_found_prod = developp(&roots[nb_root_found], found);
             polynomial* r = division_polynomials(simple_roots, root_found_prod);
             
             free_polynomial(root_found_prod);
@@ -130,23 +130,17 @@ int root_finder(char* TEST_FILE, FILE* OUTPUT_FILE, int ITERATION_LOOP, double E
         time_t t2 = clock();
         double duration = (double)(t2-t1)/1000000.;
 
-        //Maximum error
-        double max_error = -1.;
-        if(nb_root_found == nb_root){
+        if (nb_root_found == nb_root){
+            fprintf(OUTPUT_FILE,"%d;%d;%d;%lf\n",ITERATION_LOOP,nb_root,nm_iteration,duration);
             for(int i = 0; i < nb_root; i++){
-                double r = evaluate_mod(p, roots[i]);
-                if(r > max_error){
-                    max_error = r;
-                }
+                fprintf(OUTPUT_FILE, "%.15lf+i%.12lf\n", roots[i]->re, roots[i]->im);
             }
-        }else{
-            printf("Problème : %d racines trouvées sur %d.\n", nb_root_found, nb_root);
+        } else {
+            fprintf(OUTPUT_FILE, "-1\n");
         }
         
         free_complexe_array(roots, nb_root);
         free_polynomial(p);
-
-        fprintf(OUTPUT_FILE,"%d;%.12lf;%d;%d;%lf\n",ITERATION_LOOP,max_error,nb_root_found,nm_iteration,duration);
     }
 
     close_test(t);
@@ -185,6 +179,9 @@ int main(int argc, char** argv){
         int k;
         fscanf(INPUT_FILE, "%s\n", test_file);
         fscanf(INPUT_FILE, "%d\n", &k);
+
+        fprintf(OUTPUT_FILE, "New test file\n");
+        fprintf(OUTPUT_FILE, "%s\n", test_file);
 
         if(root_finder(test_file, OUTPUT_FILE, k, EPS, relaxation) != 0){
             printf("There seems to be an issue with the test file %s.\n", test_file);
